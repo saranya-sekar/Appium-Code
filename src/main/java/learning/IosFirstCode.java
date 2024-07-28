15ec498ef183cc7a9d886a31311f7aef5444bf82
lokeshkumar.thyagarajan <LokeshKumar.Thyagarajan@hidglobal.com>
2024-07-28 18:28:44 +0530

    Day 6 code added

--- src/main/java/learning/IosFirstCode.java
+++ src/main/java/learning/IosFirstCode.java
@@ -7,11 +7,14 @@ import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URISyntaxException;
 
-public class AndroidFirstCode {
+public class IosFirstCode {
     public static void main(String[] args) throws URISyntaxException, MalformedURLException, InterruptedException {
         DesiredCapabilities dc = new DesiredCapabilities();
-        dc.setCapability("platformName", "Android");
-        dc.setCapability("automationName", "UiAutomator2");
+        dc.setCapability("platformName", "iOS");
+        dc.setCapability("automationName", "XCUITest");
+        //dc.setCapability("udid","<udid>");
+        dc.setCapability("deviceName", "iPhone 15 Plus");
+        dc.setCapability("platformVersion", "17.5");
         AppiumDriver driver = new AppiumDriver(new URI("http://127.0.0.1:4723/").toURL(), dc);
         driver.quit();
     }
--- src/main/java/learning/IosFirstCodewithAppCapability.java
+++ src/main/java/learning/IosFirstCodewithAppCapability.java
@@ -7,14 +7,15 @@ import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URISyntaxException;
 
-public class IosFirstCode {
+public class IosFirstCodewithAppCapability {
     public static void main(String[] args) throws URISyntaxException, MalformedURLException, InterruptedException {
         DesiredCapabilities dc = new DesiredCapabilities();
         dc.setCapability("platformName", "iOS");
         dc.setCapability("automationName", "XCUITest");
         //dc.setCapability("udid","<udid>");
-        dc.setCapability("deviceName","iPhone 15 Plus");
-        dc.setCapability("platformVersion","17.5");
+        dc.setCapability("deviceName", "iPhone 15 Plus");
+        dc.setCapability("platformVersion", "17.5");
+        dc.setCapability("app", System.getProperty("user.dir") + "/apks/UiCatalog.zip");
         AppiumDriver driver = new AppiumDriver(new URI("http://127.0.0.1:4723/").toURL(), dc);
         driver.quit();
     }
